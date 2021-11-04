4f79bb01069d20fe954a9943bace903c661aa4da
MavielS <46908484+MavielS@users.noreply.github.com>
2021-11-04 10:54:30 +0100

    Final commit

--- fastapi/backend/back_func.py
+++ fastapi/backend/back_func.py
@@ -1,7 +1,19 @@
 import pandas as pd
 import numpy as np
+import pickle
+import shap
 
 from sklearn.preprocessing import LabelEncoder
+
+# Load all the neccessary objects
+path_rf = './backend/rf_objects/'
+
+model = pickle.load(open(path_rf+'rf_best', 'rb'))
+scaler_rf = pickle.load(open(path_rf+'scaler_rf', 'rb'))
+imputer_rf = pickle.load(open(path_rf+'imputer_rf', 'rb'))
+le_rf = pickle.load(open(path_rf+'le_rf', 'rb'))
+one_hot_rf = pickle.load(open(path_rf+'one_hot_rf', 'rb'))
+to_drop = pickle.load(open(path_rf+'to_drop', 'rb')) 
 
 def handle_outliers(df):
     ### DAYS_EMPLOYED_ANOM ###
@@ -50,3 +62,67 @@ def adding_features(df):
     print('Features added.')
     
     return df
+
+def get_prediction(sample_client, explained = False):
+
+    # Preprocess the data
+
+    # 1. Handle Outliers
+    sample_client = handle_outliers(sample_client)
+
+    # 2. Encode
+    # Label encoding
+    le = LabelEncoder()
+    for col in le_rf:
+        le.fit(sample_client[col])
+        sample_client[col] = le.transform(sample_client[col])
+
+    # One-Hot encoding of categorical variables
+    for col in one_hot_rf.feature_names_in_: # Check if all the type of the columns are corrects
+        sample_client[col] = sample_client[col].astype('object')
+
+    cat_df = one_hot_rf.feature_names_in_
+
+    cat_df_oh = pd.DataFrame(data = one_hot_rf.transform(sample_client.select_dtypes('object')).astype('int'), columns=one_hot_rf.get_feature_names_out())
+    cat_df_oh.drop(cat_df_oh.columns[cat_df_oh.columns.str.contains('nan')], axis=1, inplace=True) # The one hot considers the value 'nan' as a category, we delete them
+
+    sample_client.drop(cat_df, axis=1, inplace=True) # We remove the object features
+    # sample_client = pd.concat([sample_client, cat_df_oh], axis=1) # We add the one hot-encoded features
+    
+    sample_client[cat_df_oh.columns] = cat_df_oh.values  # pd.concat have unexpected behavior
+
+    # Quick and Dirty solution ; TODO: Find why the pd.concat change the type of DAYS_EMPLOYED_ANOM from bool to object
+    # Not the first bug I see using pd.concat
+    # sample_client.DAYS_EMPLOYED_ANOM = sample_client.DAYS_EMPLOYED_ANOM.astype('bool')
+
+    # 3. Impute NaNs 
+    imputer = imputer_rf
+
+    col_num = sample_client.select_dtypes(include=np.number).columns
+    sample_client[col_num] = imputer.transform(sample_client[col_num])
+    
+    # 4. Add features
+    sample_client = adding_features(sample_client)
+    
+    # 5. Delete low variance features
+    sample_client.drop(to_drop, axis=1, inplace=True)
+
+    before_scaling = sample_client.copy(deep=True)
+
+    # 6. Scale datas
+    col_num = sample_client.select_dtypes(include=np.number).columns
+    sample_client[col_num] = scaler_rf.transform(sample_client[col_num])
+
+    # Make the prediction
+    prediction = model.predict_proba(sample_client)
+
+    if explained:
+        
+
+        #Explainability
+        explainer = shap.TreeExplainer(model)
+        shap_value = explainer.shap_values(sample_client)
+        feature_names = sample_client.columns
+        return prediction,  explainer, shap_value, feature_names, before_scaling
+    else:
+        return prediction
